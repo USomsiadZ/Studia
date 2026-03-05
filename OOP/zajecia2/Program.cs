ef9618f76d7309bef98b227c177d0dfbd0788c8d
Hubert Parzych <hubertparzych10@gmail.com>
2026-03-05 14:59:10 +0100

    checkpoint DOM on codedex

---
+++ OOP/zajecia2/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+Wczytać klucz odpowiedzi.
+Wczytać odpowiedzi uczestników.
+Obliczyć procent poprawnych odpowiedzi dla każdej osoby.
+Zapisać wyniki do nowego pliku (wyniki.txt) w formacie:
+StreamReader readerKlucz = new StreamReader("klucz.txt");
+string liniaKlucz = readerKlucz.ReadLine();
+readerKlucz.Close();
+*/
+

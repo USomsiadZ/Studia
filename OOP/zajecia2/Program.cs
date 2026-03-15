398125228d0f59954bc423736135ab95454656ad
Hubert Parzych <hubertparzych10@gmail.com>
2026-03-15 12:42:07 +0100

    dokończenie 1 zadania z OOP

--- OOP/zajecia2/Program.cs
+++ OOP/zajecia2/Program.cs
@@ -10,34 +10,45 @@ readerKlucz.Close();
 
 #nullable enable
 using System.IO;
+using System.Collections.Generic;
 class Program
 {
     static void Main()
     {
-        StreamReader readerKlucz = new("/Users/hubert/Github/studia/OOP/zajecia2/klucz.txt");
-        string? liniaKlucz = readerKlucz.ReadLine() ?? throw new Exception("Nie udało się odczytać klucza");
+        string katalog = "/Users/hubert/Github/studia/OOP/zajecia2";
+        string sciezkaKlucz = Path.Combine(katalog, "klucz.txt");
+        string sciezkaOdpowiedzi = Path.Combine(katalog, "odpowiedzi.txt");
+        string sciezkaWyniki = Path.Combine(katalog, "wyniki.txt");
 
-        StreamReader readerOdpowiedzi = new("/Users/hubert/Github/studia/OOP/zajecia2/odpowiedzi.txt");
-        string[] liniaOdpowiedzi = new string[64];
+        StreamReader readerKlucz = new(sciezkaKlucz);
+        string? liniaKlucz = readerKlucz.ReadLine();
+        if (liniaKlucz == null)
+        {
+            readerKlucz.Close();
+            throw new Exception("Nie udało się odczytać klucza");
+        }
 
-        int dlugosc = 0;
+        StreamReader readerOdpowiedzi = new(sciezkaOdpowiedzi);
+        //string[] liniaOdpowiedzi = new string[64];
+        List<string> liniaOdpowiedzi = [];
+
+
         for (int i = 0; true; i++)
         {
             string? wczytana = readerOdpowiedzi.ReadLine();
             if (wczytana == null) break;
-            liniaOdpowiedzi[i] = wczytana;
-            dlugosc++;
+            liniaOdpowiedzi.Add(wczytana);
         }
-        if (liniaOdpowiedzi[0] == null) throw new Exception("Nie udało się wczytać odpowiedzi");
+        if (liniaOdpowiedzi.Count == 0) throw new Exception("Nie udało się wczytać odpowiedzi");
+
         int poprawne;
         string[] liniaKlucz_tab = liniaKlucz.Trim().Split(" ");
-        string sciezkaWyniki = "/Users/hubert/Github/studia/OOP/zajecia2/wyniki.txt";
         File.WriteAllText(sciezkaWyniki, "");
-        for (int j = 0; j < dlugosc; j++)
+        for (int j = 0; j < liniaOdpowiedzi.Count; j++)
         {
             poprawne = 0;
             string[] liniaOdpowiedzi_tab = liniaOdpowiedzi[j].Trim().Split(" ");
-
+            if (liniaOdpowiedzi_tab.Length != liniaKlucz_tab.Length + 2) throw new Exception("Error - dlugosc klucza i odpowiedzi jest różna");
             for (int i = 0; i < liniaKlucz_tab.Length; i++)
             {
                 if (liniaKlucz_tab[i] == liniaOdpowiedzi_tab[i + 2]) poprawne++;
@@ -46,11 +57,12 @@ class Program
 
             Console.WriteLine(liniaOdpowiedzi_tab[0] + " " + liniaOdpowiedzi_tab[1]);
             Console.WriteLine("Ilość poprawnych: " + poprawne);
-            Console.WriteLine("Procent: " + poprawne * 10 + "%");
+            Console.WriteLine("Procent: " + poprawne * 100.0 / liniaKlucz_tab.Length + "%");
             Console.WriteLine("\n");
 
-            string tekst = liniaOdpowiedzi_tab[0] + " " + liniaOdpowiedzi_tab[1] + "\n" + "Ilość poprawnych: " + poprawne + "\n" + "Procent: " + (poprawne * 10) + "%";
-            File.AppendAllText(sciezkaWyniki, tekst + "\n");
+            int procent = (int)Math.Round(poprawne * 100.0 / liniaKlucz_tab.Length);
+            string tekst = $"{liniaOdpowiedzi_tab[0]} {liniaOdpowiedzi_tab[1]} {procent}%";
+            File.AppendAllText(sciezkaWyniki, tekst + '\n');
         }
 
 
